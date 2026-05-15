6709fdb9c024333515eb5f50be6d98a8f73049f4
hassaninrada <213332777+hassaninrada@users.noreply.github.com>
2026-05-15 16:23:41 +0500

    Delete teacher-hub.js

--- teacher-hub.js
+++
@@ -1,7 +0,0 @@
-const logoutBtn = document.getElementById("logoutBtn");
-if (logoutBtn) {
-    logoutBtn.addEventListener("click", () => {
-        localStorage.clear();
-        window.location.href = "index.html";
-    });
-}

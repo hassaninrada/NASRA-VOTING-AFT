6e41fdb9037490ff937377fe8e6a71e5fc7addc3
hassaninrada <213332777+hassaninrada@users.noreply.github.com>
2026-05-20 18:28:36 +0500

    Update supabase-init.js

--- supabase-init.js
+++ supabase-init.js
@@ -1,9 +1,11 @@
 
 
+
+
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 // Supabase Project URL and Anon Key
 const supabaseUrl = 'https://adqmsvilzisqwttkpzww.supabase.co'
 const supabaseKey = 'sb_publishable_l0_0YEWU1V4zdyho8UxHKQ_BZq4Fxql'
-export const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const supabase = createClient(supabaseUrl, supabaseKey)
